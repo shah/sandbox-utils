42376f20827104f4bd1e368065deec24d659d412
System Manager <manager@itweb.(none)>
2009-08-08 14:01:05 -0400

    Added tomcat and ant setup scripts

---
+++ java/setup-ant.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Install Java Developers Kit"
+sudo apt-get install -y sun-java6-jdk
+
+export SETUP_ANT_VERSION=1.7.1
+wget http://apache.promopeddler.com/ant/binaries/apache-ant-$SETUP_ANT_VERSION-bin.tar.gz
+tar -xzf apache-ant-$SETUP_ANT_VERSION-bin.tar.gz
+sudo mv apache-ant-$SETUP_ANT_VERSION /usr/share/ant
+sudo ln -s /usr/share/ant/bin/ant /usr/bin/ant
+
+echo "Setup Ant $SETUP_ANT_VERSION in /usr/share/ant."
+ls -l /usr/bin/ant
---
+++ java/setup-tomcat6.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Install Java Developers Kit"
+sudo apt-get install -y sun-java6-jdk
+
+export SETUP_TOMCAT_VERSION_MAJOR=6
+export SETUP_TOMCAT_VERSION=6.0.20
+wget http://apache.promotionalpromos.com/tomcat/tomcat-$SETUP_TOMCAT_VERSION_MAJOR/v$SETUP_TOMCAT_VERSION/bin/apache-tomcat-$SETUP_TOMCAT_VERSION.tar.gz
+tar xzf apache-tomcat-$SETUP_TOMCAT_VERSION.tar.gz
+sudo mv apache-tomcat-$SETUP_TOMCAT_VERSION /usr/share/tomcat$SETUP_TOMCAT_VERSION_MAJOR
+sudo ln -s /usr/share/tomcat$SETUP_TOMCAT_VERSION_MAJOR /usr/share/tomcat
+
+echo "Setup Tomcat $SETUP_TOMCAT_VERSION in /usr/share/tomcat$SETUP_TOMCAT_VERSION_MAJOR."
+ls -l /usr/share/tomcat
